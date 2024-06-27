e36fcdb802b5b6f8801870d50a80d4afe57db42e
munikumar.mm <munikumar.m09@gmail.com>
2024-06-27 22:20:42 +0530

    fix format and lint errors

--- tests/database/sqlite/crud/test_smartapi_crud.py
+++ tests/database/sqlite/crud/test_smartapi_crud.py
@@ -1,26 +1,37 @@
+"""" 
+This module contains tests for the smartapi_crud.py module in the sqlite/crud directory.
+"""
+from pathlib import Path
+
 import pytest
-from app.database.sqlite.sqlite_db_connection import get_session
+
 from app.database.sqlite.crud.smartapi_curd import (
-    get_smartapi_tokens_by_any_condition,
     get_smartapi_tokens_by_all_conditions,
+    get_smartapi_tokens_by_any_condition,
 )
 from app.database.sqlite.models.smartapi_models import SmartAPIToken
+from app.database.sqlite.sqlite_db_connection import get_session
 from app.utils.startup_utils import create_smartapi_tokens_db
 from app.utils.urls import SQLITE_DB_URL
-from pathlib import Path
 
 
 @pytest.fixture
 def session():
+    """
+    Fixture to get the session object.
+    """
     return next(get_session())
 
 
-def test_get_smartapi_tokens_by_any_condition(session):
+def test_get_smartapi_tokens_by_any_and_all_condition(session):
+    """
+    Test the get_smartapi_tokens_by_any_condition
+    """
     db_file_path = Path(SQLITE_DB_URL.split("sqlite:///")[-1])
     remove_at_end = not db_file_path.exists()
 
     create_smartapi_tokens_db(True)
-    
+
     # Insert test data
     token1 = SmartAPIToken(
         name="INFY",
--- tests/database/sqlite/test_sqlite_db_connection.py
+++ tests/database/sqlite/test_sqlite_db_connection.py
@@ -2,29 +2,31 @@
 Test the database connection and interaction with the SQLite database.
 """
 
+from pathlib import Path
+
 from sqlmodel import inspect, select
+
+from app.database.sqlite.models.smartapi_models import SmartAPIToken
 from app.database.sqlite.sqlite_db_connection import (
     create_db_and_tables,
     get_session,
     sqlite_engine,
 )
-from app.database.sqlite.models.smartapi_models import SmartAPIToken
-from pathlib import Path
 from app.utils.urls import SQLITE_DB_URL
 
 table_names = ["smartapitoken"]
 
 
 def test_database_init_and_interaction():
-    """ 
+    """
     Test if the database is created and tables are created and empty and able to interact with the database.
     """
-    
+
     db_file_path = Path(SQLITE_DB_URL.split("sqlite:///")[-1])
     remove_at_end = not db_file_path.exists()
-    
+
     create_db_and_tables()
-    
+
     # Check if the session is active
     session = next(get_session())
     assert session.is_active
@@ -41,9 +43,6 @@ def test_database_init_and_interaction():
         session.exec(select(SmartAPIToken)).all()
     except Exception as e:
         assert False, f"Failed to interact with the database: {e}"
-    
+
     if remove_at_end:
         db_file_path.unlink()
-    
-    
-    
