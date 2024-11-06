7897fea2adbcbe0a934bed036e447c2741ea1065
munikumar.mm <munikumar.m09@gmail.com>
2024-11-06 08:51:11 +0530

    remove authentication related code

--- app/data_layer/database/models/__init__.py
+++ app/data_layer/database/models/__init__.py
@@ -1,3 +1,2 @@
 from .smartapi_model import *
-from .user_model import *
 from .websocket_model import *
--- main.py
+++ main.py
@@ -5,14 +5,12 @@ from app.routers.nse.derivatives import derivatives
 from app.routers.nse.equity import equity
 from app.routers.smartapi.smartapi import smartapi
 from app.utils.startup_utils import create_smartapi_tokens_db
-from app.routers.authentication import authentication
 
 app = FastAPI()
 
 app.include_router(derivatives.router)
 app.include_router(equity.router)
 app.include_router(smartapi.router)
-app.include_router(authentication.router)
 
 
 @app.on_event("startup")
